1c887e554f892e1176e539d5833687f515c20ceb
newvegas-2016 <57021950+newvegas-2016@users.noreply.github.com>
2023-02-26 20:28:00 +0800

    添加项目文件。

---
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Models;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository repository;
+        public int Pagesize = 4;
+
+        public ProductController(IProductRepository productRepository)
+        {
+            this.repository = productRepository;
+        }
+
+        public ViewResult List(string category,int page = 1)
+        {
+            ProductsListViewModel model = new ProductsListViewModel
+            {
+                Products = repository.Products
+                .Where(p => category == null || p.Category == category)
+                .OrderBy(p => p.ProductID)
+                .Skip((page - 1) * Pagesize)
+                .Take(Pagesize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = Pagesize,
+                    TotalItems = category==null ? repository.Products.Count() : repository.Products.Where(e=>e.Category==category).Count()
+                },
+                CurrentCategory = category
+            };
+            return View(model);
+        }
+
+        public FileContentResult GetImage(int productId)
+        {
+            Product prod = repository.Products.FirstOrDefault(p => p.ProductID == productId);
+            if (prod != null)
+            {
+                return File(prod.ImageData, prod.ImageMimeType);
+            }
+            else { return null; }
+        }
+    }
+}
